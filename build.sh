e2d4f7593925762d352dea2ed3812964e5cb693a
Matt Bailey <36735446+MattBaileyDesign@users.noreply.github.com>
2020-02-25 16:56:54 -0500

    Add files via upload
    Change notes from V 1.003:
    
    • Adjusted instance locations to accomodate current uses or ExtraBold and Light weights
    • Slightly redrew a handful of characters (/C, /G, /J, /S, /AE, /a.1, /c, /e, /j, /t, /2, /3, /question, /questiondown, /Omega, /Delta, /Germandbls, /product, /summation /acute, /commaaccent, /commaaccentturnedabove, /grave, /tilde)
    • Checked for symmetry across weights
    • Changed some diacritics using /ogonek to have a smooth connection
    • Normalized x-height to 1024 across all weights
    • Proportionally scaled numerators and denominators, redrawing fractions
    • Set linked sidebearings and componenets where applicable in source files

---
+++ build.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+# -------------------------------------------------------------------
+# UPDATE THIS VARIABLE ----------------------------------------------
+
+# thisFont="Spartan" # must match the name in the font file, e.g. FiraCode-VF.ttf needs the variable "FiraCode"
+
+# -------------------------------------------------------------------
+# Update the following as needed ------------------------------------
+set -e
+
+
+echo "Generating VFs"
+# mkdir -p ./fonts/variable
+fontmake -g source/Spartan.glyphs -o variable --round-instances --output-path ./fonts/Spartan\[wght\].ttf
+
+
+
+echo "Generating Statics" ##run fixes on statics as well. 
+
+fontmake -g source/Spartan.glyphs -o ttf --round-instances -a --keep-direction -i --output-dir ./fonts/static/
+
+
+echo "Statics Built" ##run fixes on statics as well. 
+
+echo "Removing Build UFOS"
+
+rm -rf master_ufo/ instance_ufo/
+
+echo "Build UFOS Removed"
+
+echo "Post processing"
+
+vfs=$(ls ./fonts/*.ttf)
+for vf in $vfs
+do
+	gftools fix-dsig -f $vf;
+	gftools fix-nonhinting $vf "$vf.fix";
+	mv "$vf.fix" $vf;
+	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
+	rtrip=$(basename -s .ttf $vf)
+	new_file=./fonts/$rtrip.ttx;
+	rm $vf;
+	ttx $new_file
+	rm ./fonts/*.ttx
+done
+rm ./fonts/*backup*.ttf
+
+
+stat=$(ls ./fonts/static/*.ttf)
+for st in $stat
+do
+	gftools fix-dsig -f $st;
+	gftools fix-hinting $st; #if this doesn't work rename as done above and figure diff solution.
+	mv "$st.fix" $st;
+	ttx -f -x "MVAR" $st; # Drop MVAR. Table has issue in DW
+	rtrip=$(basename -s .ttf $st)
+	new_file=./fonts/static/$rtrip.ttx;
+	rm $st;
+	ttx $new_file
+	rm ./fonts/static/*.ttx
+done
+# rm ./fonts/vf/static/*backup*.ttf
+
+
+
+
+
+echo "fix vf meta? ok let's try that. I'm trying to fix VF meta"
+gftools fix-vf-meta $vfs;
+for vf in $vfs
+do
+	mv "$vf.fix" $vf;
+done
+
+echo "Post processing complete"
+
+# echo "running fb"
+# fontbakery check-googlefonts ./fonts/vf/*.ttf --ghmarkdown MohaveChecks.md 
+# echo "finish vf check"
+# echo "show dir now run static fb checks"
+# fontbakery check-googlefonts ./fonts/vf/static/*.ttf --ghmarkdown Mohave_ttf_checks.md
+# echo "fb checks done"
